72b564db649512e707b594f19fdd2ebe264b773c
AHorbunov-EPAM <Artem_Horbunov1@epam.com>
2025-08-26 20:57:14 +0300

    Revert "Update TF to use Blob storage"
    This reverts commit da57033ab92c996f00d75ff92547f044f559f6f7.

--- iac/terraform.tf
+++ iac/terraform.tf
@@ -1,15 +1,11 @@
 terraform {
-  backend "azurerm" {
-    resource_group_name  = "ToDoApp"
-    storage_account_name = "todoappterraformstate"
-    container_name       = "tfstate"
-    key                  = "terraform.tfstate"
-  }
+  backend "local" {}
 
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
       version = "4.33.0"
+
     }
   }
 }
