fa6857e27bd15822c2f741a7356f30cb949b1923
AHorbunov-EPAM <Artem_Horbunov1@epam.com>
2025-08-27 20:30:42 +0300

    Add Azure App Service Plan and Web App functionality
    This commit introduces the ability to create an Azure App Service Plan and a Windows Web App within an Azure Resource Group. It includes new imports for Azure Web resources, sets up an App Service Plan with a Free SKU, and configures the Web App with HTTPS enforcement and IP security restrictions. The Web App's URL, along with the resource group name and web app name, is also exported.

--- ToDo.Azure/Program.cs
+++ ToDo.Azure/Program.cs
@@ -2,6 +2,8 @@
 using Pulumi.AzureNative.Resources;
 using Pulumi.AzureNative.Storage;
 using Pulumi.AzureNative.Storage.Inputs;
+using Pulumi.AzureNative.Web;
+using Pulumi.AzureNative.Web.Inputs;
 using System.Collections.Generic;
 
 return await Pulumi.Deployment.RunAsync(() =>
@@ -9,6 +11,66 @@ return await Pulumi.Deployment.RunAsync(() =>
     // Create an Azure Resource Group
     var resourceGroup = new ResourceGroup("ToDoHel", new ResourceGroupArgs
     {
-        ResourceGroupName = "ToDoHel"
+        ResourceGroupName = "ToDoHel",
+        Location = "East US"
     });
+
+    // Create an App Service Plan for Windows
+    var appServicePlan = new AppServicePlan("ToDoAppServicePlan", new AppServicePlanArgs
+    {
+        Name = "ToDoAppServicePlan",
+        ResourceGroupName = resourceGroup.Name,
+        Location = resourceGroup.Location,
+        Kind = "Windows",
+        Sku = new SkuDescriptionArgs
+        {
+            Name = "F1",
+            Tier = "Free"
+        }
+    });
+
+    // Create a Windows Web App
+    var webApp = new WebApp("helenatodoapp", new WebAppArgs
+    {
+        Name = "helenatodoapp",
+        ResourceGroupName = resourceGroup.Name,
+        Location = resourceGroup.Location,
+        ServerFarmId = appServicePlan.Id,
+        HttpsOnly = true,
+        SiteConfig = new SiteConfigArgs
+        {
+            AlwaysOn = false,
+            NetFrameworkVersion = "v9.0",
+            IpSecurityRestrictions = new[]
+            {
+                new IpSecurityRestrictionArgs
+                {
+                    IpAddress = "Any",
+                    Action = "Deny"
+                }
+            },
+            ScmIpSecurityRestrictions = new[]
+            {
+                new IpSecurityRestrictionArgs
+                {
+                    IpAddress = "Any",
+                    Action = "Deny"
+                }
+            },
+            IpSecurityRestrictionsDefaultAction = "Deny",
+            ScmIpSecurityRestrictionsDefaultAction = "Deny"
+        },
+        Identity = new ManagedServiceIdentityArgs
+        {
+            Type = Pulumi.AzureNative.Web.ManagedServiceIdentityType.SystemAssigned
+        }
+    });
+
+    // Export the Web App URL
+    return new Dictionary<string, object?>
+    {
+        ["resourceGroupName"] = resourceGroup.Name,
+        ["webAppName"] = webApp.Name,
+        ["webAppUrl"] = webApp.DefaultHostName.Apply(hostname => $"https://{hostname}")
+    };
 });
