51204fa361b5fd60394d93185bf3ec031e9798c7
AHorbunov-EPAM <Artem_Horbunov1@epam.com>
2025-08-27 20:15:21 +0300

    Update project structure and configuration for Azure
    - Enhanced `.gitignore` to exclude Visual Studio and Azure-related files.
    - Modified `Program.cs` to implement Azure Resource Group creation.
    - Set Azure location in `Pulumi.dev.yaml` to "polandcentral".
    - Created `Pulumi.yaml` for project configuration and metadata.
    - Added `README.md` with an overview and instructions for the Pulumi template.
    - Created `ToDo.Azure.csproj` to define project structure and dependencies.
    - Updated `ToDo.sln` to include the new project.
    - Modified `terraform.tfstate.backup` to reflect new managed resources.
    - Expanded `README.md` with Terraform infrastructure setup instructions.
    - Changed `terraform.tf` to use Azure Blob Storage for remote state management.

---
+++ ToDo.Azure/Program.cs
@@ -0,0 +1,14 @@
+﻿using Pulumi;
+using Pulumi.AzureNative.Resources;
+using Pulumi.AzureNative.Storage;
+using Pulumi.AzureNative.Storage.Inputs;
+using System.Collections.Generic;
+
+return await Pulumi.Deployment.RunAsync(() =>
+{
+    // Create an Azure Resource Group
+    var resourceGroup = new ResourceGroup("ToDoHel", new ResourceGroupArgs
+    {
+        ResourceGroupName = "ToDoHel"
+    });
+});
